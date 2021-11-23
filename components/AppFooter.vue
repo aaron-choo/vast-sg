b51459617c4fa07595a02ec6ab6f6f659bc6b84e
Aaron Choo <aaronchoo@gmail.com>
2021-11-23 22:54:06 +0800

    feat: added footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,33 @@
+<template>
+<footer class="footer overflow-hidden z-0 relative mt-24">
+    <div class="absolute bottom-4 left-4 right-4 w-auto flex justify-between z-10">
+        <nuxt-link to="/" class="copyright">
+        <Logo class="logo w-16 transform translate-x-14"/>
+        </nuxt-link>
+    </div>
+    <div class="opacity-10 absolute bottom-0 w-full z-0">
+        <Logo class="w-full transform translate-y-1/2"/>
+    </div>
+</footer>
+</template>
+
+<script>
+import Logo from '@/components/Logo';
+export default {
+    name: 'AppHeader',
+    components: {
+        Logo
+    }
+}
+</script>
+<style scoped>
+footer{
+    min-height:100vh;
+}
+.logo::before{
+    content:"© 2021";
+    position:absolute;
+    bottom:-0.2em;
+    left: -3.5em;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,16 +3,19 @@
         <AppHeader @close-menu="menuOpen = false"/>
         <nuxt />
         <AppCursor/>
+        <AppFooter/>
     </div>
 </template>
 
 <script>
 import AppHeader from '../components/AppHeader';
+import AppFooter from '../components/AppFooter';
 import AppCursor from '../components/AppCursor';
 
 export default {
     components: {
         AppHeader,
+        AppFooter,
         AppCursor
     }
 }
