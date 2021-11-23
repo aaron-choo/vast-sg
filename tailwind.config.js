a096c6f8f97c881c36dc65c16fa1b8c3fd48f7df
Aaron Choo <aaronchoo@gmail.com>
2021-11-23 15:12:27 +0800

    feat: added tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+    purge: [
+        './slices/**/*.vue',
+        './components/**.vue',
+        './layouts/**.vue',
+        './pages/**.vue',
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
