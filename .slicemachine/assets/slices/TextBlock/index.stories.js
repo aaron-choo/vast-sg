0bf3159c8740441ecb67c6542cfbef0c713b22b4
Aaron Choo <aaronchoo@gmail.com>
2021-11-27 00:44:26 +0800

    chore: updated slices

--- .slicemachine/assets/slices/TextBlock/index.stories.js
+++ .slicemachine/assets/slices/TextBlock/index.stories.js
@@ -18,7 +18,7 @@ export const _DefaultSlice = () => ({
   },
   data() {
     return {
-      mock: {"variation":"default-slice","name":"Default slice","slice_type":"text_block","items":[],"primary":{"title":[{"type":"heading1","text":"Aggregate out-of-the-box models","spans":[]}],"description":[{"type":"paragraph","text":"Officia est quis laborum nisi et aliqua nisi eu.","spans":[]}]},"id":"_DefaultSlice"}
+      mock: {"variation":"default-slice","name":"Default slice","slice_type":"text_block","items":[],"primary":{"title":[{"type":"heading1","text":"Reintermediate user-centric convergence","spans":[]}],"description":[{"type":"paragraph","text":"Sint tempor irure fugiat aute nulla proident irure ad. Proident quis sit exercitation duis qui dolore.","spans":[]}]},"id":"_DefaultSlice"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
