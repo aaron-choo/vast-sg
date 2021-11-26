94ee08b12e86d9dab78ff489a35e0f11795f3c0c
Aaron Choo <aaronchoo@gmail.com>
2021-11-27 00:43:56 +0800

    feat: updated project page template

--- pages/projects/_uid.vue
+++ pages/projects/_uid.vue
@@ -3,14 +3,22 @@
     <article>
       <div class="project-header">
         <div class="w-full">
-          <h1 class="w-10/12 tracking-tighter text-5xl lg:text-9xl px-4 pt-16">{{ $prismic.asText(title) }}
-          <br/>
-          </h1>
-          <h1 v-for="tag in tags" :key="tag" class="w-10/12 uppercase font-light tracking-tighter text-5xl lg:text-9xl px-4">
-              {{ tag }}
-          </h1>
-          <div v-if="image.url !== undefined" class="post-image pt-8">
-            <img :src="image.url"/>
+          <div id="header-text" class="w-10/12 flex flex-col justify-center p-4 mt-10">
+            <h1 class="tracking-tighter text-5xl lg:text-8xl">{{ $prismic.asText(title) }}</h1>
+            <h1 class="font-light inline-block tag tracking-tighter capitalize text-5xl lg:text-5xl">
+              <span v-for="(tag, index) in tags" :key="tag">
+                  {{ tag }}
+                  <template v-if="Object.keys(tags).length > 1">
+                    <span v-if="index != Object.keys(tags).length - 1">+</span>
+                  </template>
+              </span>
+            </h1>
+          </div>
+          <div v-if="heroimage.url !== undefined" class="project-image px-4">
+            <img :src="heroimage.url" class="rounded-2xl"/>
+          </div>
+          <div class="description-wrapper px-4">
+            <p class="description tracking-tighter uppercase text-5xl lg:text-4xl">{{$prismic.asText(description)}}</p>
           </div>
         </div>
       </div>
@@ -38,14 +46,15 @@ export default {
         // Returns data to be used in template
         return {
         page: project,
-        title: project.data.project_title,
-        date: project.data.project_date,
-        description: project.data.project_description,
-        image: project.data.project_image,
-        color1: project.data.project_color_1,
-        color2: project.data.project_color_2,
+        title: project.data.title,
+        date: project.date,
+        description: project.data.description,
+        heroimage: project.data.image,
+        backgroundColor: project.data.background_color,
+        textColor: project.data.text_color,
         tags: project.tags
         }
+
     } catch (e) {
         // Returns error page
         error({ statusCode: 404, message: 'Page not found' })
@@ -53,23 +62,41 @@ export default {
   },
   head () {
     return {
-      title: this.$prismic.asText(this.page.data.meta_title),
+      title: this.$prismic.asText(this.page.data['meta-title']),
       meta: [
         {
           hid: 'description',
           name: 'description',
           content: this.$prismic
-            .asText(this.page.data.meta_description)
+            .asText(this.page.data['meta-description'])
             .substring(0, 158)
         }
       ]
+    }
+  },
+  mounted() {
+    document.documentElement.style.setProperty('--bg', this.page.data['background-color']);
+    document.documentElement.style.setProperty('--color-primary', this.page.data['text-color']);
+    document.documentElement.style.setProperty('--color', this.page.data['text-color']);
+    window.addEventListener('scroll', this.headerScroll);
+  },
+  destroyed() {
+    document.documentElement.style.setProperty('--bg', '');
+    document.documentElement.style.setProperty('--color-primary', '');
+    document.documentElement.style.setProperty('--color', '');
+  },
+  methods: {
+    headerScroll () {
+      document.getElementById('header-text').style.transform = "translateY(" + document.documentElement.scrollTop/1.5 + "px)"
     }
   }
 }
 </script>
 <style scoped>
-.scope {
-  text-indent: 1em;
-  display: inline-block;
+#header-text {  
+  height: 50vh;
+}
+#header-text *{
+  line-height: .8;
 }
 </style>
