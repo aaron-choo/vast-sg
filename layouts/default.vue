af829ad6781c22f47c98f95730267646e3bdaa92
Aaron Choo <aaronchoo@gmail.com>
2021-11-29 01:11:24 +0800

    feat: added noise baclground

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="site-container">
         <AppHeader/>
         <nuxt />
         <AppCursor/>
@@ -20,3 +20,30 @@ export default {
     }
 }
 </script>
+<style scoped>
+.site-container::before {
+    animation: grain 8s steps(10) infinite;
+    background-image: url(/noise.png);
+    content: "";
+    height: 300%;
+    left: -50%;
+    opacity: .65;
+    position: fixed;
+    top: -100%;
+    width: 300%;
+    pointer-events: none;
+    z-index: -1;
+}
+@keyframes grain {
+  0%, 100% { transform:translate(0, 0); }
+  10% { transform:translate(-5%, -10%); }
+  20% { transform:translate(-15%, 5%); }
+  30% { transform:translate(7%, -25%); }
+  40% { transform:translate(-5%, 25%); }
+  50% { transform:translate(-15%, 10%); }
+  60% { transform:translate(15%, 0%); }
+  70% { transform:translate(0%, 15%); }
+  80% { transform:translate(3%, 25%); }
+  90% { transform:translate(-10%, 10%); }
+}
+</style>
