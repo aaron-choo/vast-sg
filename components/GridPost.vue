f573b57d709a4d693dcbfc260afd703072603edd
Aaron Choo <aaronchoo@gmail.com>
2021-11-29 01:14:41 +0800

    feat: updated grid post component

--- components/GridPost.vue
+++ components/GridPost.vue
@@ -1,25 +1,28 @@
 <template>
   <section class="">
     <nuxt-link :to="LinkGetter(projectdata)">
-      <div v-if="projectdata.data.image.url !== undefined" class="main_image">
-        <ResponsiveImg :imgobject="projectdata.data.image" :sizes="imgsize" />
+      <div v-if="projectdata.data.image.url !== undefined" class="overflow-hidden">
+        <nuxt-img v-if="projectdata.data.image.url" :src="projectdata.data.image.url" sizes="sm:100vw md:100vw lg:100vw xl:50vw 2xl:50vw" class="image transition duration-1000" />
       </div>
 
-      <div class="px-8 py-4 text">
-        <div class="text-sm italic date">
+      <div class="text my-2">
+        <!-- <div class="text-sm italic date">
           {{
           Intl.DateTimeFormat('en-US').format(
           new Date(projectdata.first_publication_date)
           )
           }}
-        </div>
+        </div> -->
 
-        <div class="mt-1 mb-4 text-xl font-bold title">
-          {{ $prismic.asText(projectdata.data.title) }}
+        <div class="text-2xl uppercase title">
+          {{ $prismic.asText(projectdata.data.title) }} (<span v-for="(tag, index) in projectdata.tags" :key="tag" class="inline-block">{{ tag }}<template v-if="Object.keys(projectdata.tags).length > 1"><span v-if="index != Object.keys(projectdata.tags).length - 1">/</span></template>
+          </span>)
         </div>
 
         <div v-if="projectdata.data.summary.length > 0" class="text-xs summary">
+          
           <prismic-rich-text :field="projectdata.data.summary" />
+          
         </div>
       </div>
     </nuxt-link>
@@ -27,12 +30,10 @@
 </template>
 
 <script>
-import ResponsiveImg from '~/components/ResponsiveImg'
 import LinkResolver from '~/plugins/link-resolver.js'
 export default {
   name: 'GridPost',
   components: {
-    ResponsiveImg
   },
   props: {
     projectdata: {
@@ -51,3 +52,11 @@ export default {
   }
 }
 </script>
+<style scoped>
+.image{
+  width: 100%;
+}
+.image:hover{
+  transform:scale(1.02);
+}
+</style>
