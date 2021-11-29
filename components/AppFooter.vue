805551d7551038763c0b9e004ea5fc11ca2776a9
Aaron Choo <aaronchoo@gmail.com>
2021-11-30 04:58:02 +0800

    chore: updated app footer component

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,11 +1,11 @@
 <template>
-<footer class="footer overflow-hidden z-0 relative mt-24">
+<footer id="footer" class="footer overflow-hidden relative">
     <div class="absolute bottom-4 left-4 right-4 w-auto flex justify-between z-10">
         <nuxt-link to="/" class="copyright">
         <Logo class="logo w-16 transform translate-x-14"/>
         </nuxt-link>
     </div>
-    <div class="opacity-10 absolute bottom-0 w-full z-0">
+    <div class="opacity-0 absolute bottom-0 w-full z-0">
         <Logo class="w-full transform translate-y-1/2"/>
     </div>
 </footer>
@@ -14,20 +14,23 @@
 <script>
 import Logo from '@/components/Logo';
 export default {
-    name: 'AppHeader',
+    name: 'AppFooter',
     components: {
         Logo
     }
 }
 </script>
 <style scoped>
-/* footer{
-    min-height:100vh;
+footer{
+    min-height:50vh;
+    color: #fff;
+    background-color: #000;
+    z-index: -2;
 }
 .logo::before{
     content:"© 2021";
     position:absolute;
     bottom:-0.2em;
     left: -3.5em;
-} */
+}
 </style>
