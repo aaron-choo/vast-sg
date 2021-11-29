79bc1eadf81b79583ce66eb33c85db37d7b019c0
Aaron Choo <aaronchoo@gmail.com>
2021-11-30 04:57:46 +0800

    chore: updated image block component

--- components/slices/ImageBlock/index.vue
+++ components/slices/ImageBlock/index.vue
@@ -1,9 +1,9 @@
 <template>
 <div :class="slice.variation">
-  <div class="image-block section my-24 mx-4 lg:mx-40 grid gap-4" :class="'lg:grid-cols-'+slice.primary.columns">
+  <div class="image-block section my-24 mx-4 lg:mx-40 grid gap-4" :class="'grid-cols-'+slice.primary.columns">
     <div v-for="item in slice.items" :key="item" class="relative">
       <nuxt-img v-if="item.image.url" :src="item.image.url" sizes="sm:640px md:768px lg:1024px xl:1280px 2xl:1536px" class="image"/>
-      <prismic-rich-text :field="item.caption" class="image-caption font-light uppercase text-sm" />
+      <prismic-rich-text :field="item.caption" class="image-caption uppercase text-sm" />
     </div>
   </div>
 </div>
