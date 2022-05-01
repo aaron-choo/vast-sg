a5eac74288a3f626ec40628d61193d62a96f82a8
Aaron Choo <aaronchoo@gmail.com>
2022-05-02 01:23:26 +0800

    feat: upated contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,42 +1,76 @@
 <template>
   <main>
     <section class="contact-header z-0 overflow-hidden">
-      <div
-        id="header-text"
-        class="w-full flex flex-col justify-center p-4 py-52 pt-60 relative"
-      >
-        <h1
-          id="header-title"
-          class="tracking-tight text-5xl lg:text-8xl uppercase mb-4"
-        >
-          <span
-            v-for="(word, index) in titleWords"
-            :key="index"
-            class="title-words inline-block overflow-hidden"
-            ><span
-              v-for="(letter, index2) in Array.from(word)"
-              :key="index2"
-              class="inline-block"
-              >{{ letter }}</span
-            >&nbsp;</span
-          >
+      <div id="header-text" class="w-full flex flex-col justify-center p-4 py-52 pt-60 relative">
+        <h1 id="header-title" class="tracking-tight text-5xl lg:text-8xl uppercase mb-4">
+          <span v-for="(word, index) in titleWords" :key="index" class="title-words inline-block overflow-hidden"><span
+              v-for="(letter, index2) in Array.from(word)" :key="index2" class="inline-block">{{ letter
+              }}</span>&nbsp;</span>
         </h1>
-        <p
-          id="header-description"
+        <p id="header-description"
           class="inline-block tag text-2xl lg:text-3xl transition duration-300 leading-3 transform"
-          :class="{ 'has-scroll-over': scrollOver }"
-        >
-          <span class="intro text-sm lg:text-base uppercase inline-block mr-16"
-            >({{ $prismic.asText(intro) }})</span
-          ><span class="description serif leading-7 font-light"
-            ><span
-              v-for="(word, index) in descriptionWords"
-              :key="index"
-              class="description-words inline-block"
-              >{{ word }}</span
-            ></span
-          >
+          :class="{ 'has-scroll-over': scrollOver }">
+          <span class="intro text-sm lg:text-base uppercase inline-block mr-16">({{ $prismic.asText(intro)
+          }})</span><span class="description serif leading-7 font-light"><span
+              v-for="(word, index) in descriptionWords" :key="index" class="description-words inline-block">{{ word
+              }}</span></span>
         </p>
+      </div>
+    </section>
+    <section class="content">
+      <div class="m-4 mb-24 grid gap-8 md:grid-cols-2 text-3xl lg:text-4xl font-light uppercase">
+        <div class="grid gap-8">
+          <div>
+            <p class="text-sm lg:text-base uppercase">(Address)</p>
+            <a class="inline-block" href="https://maps.google.com/?q=VAST%20632%20Veerasamy%20Road%20#19-102"
+              target="_blank" aria-label="View on map" rel="noopener">
+              <h2>Vast Design</h2>
+              <p>1°18’22”N<br>
+                103°51’16”E</p>
+            </a>
+          </div>
+        </div>
+        <div class="grid gap-8">
+          <div>
+            <p class="text-sm lg:text-base uppercase">(Phone)</p>
+            <a class="inline-block" href="tel:+6569424062" aria-label="Call us" rel="noopener">
+              <h2>+65 6942 4062</h2>
+            </a>
+          </div>
+          <div>
+            <p class="text-sm lg:text-base uppercase">(Electronic Mail)</p>
+            <a class="inline-block" href="mailto:hello@vast.sg" aria-label="Email us" rel="noopener">
+              <h2>hello@vast.sg</h2>
+            </a>
+          </div>
+          <div>
+            <p class="text-sm lg:text-base uppercase">(Follow Us)</p>
+            <a class="inline-block" href="https://facebook.com/vast.sg" target="_blank"
+              aria-label="Connect with us on Facebook" tabindex="0" tabposition="bottom" rel="noopener">
+              <h2>Facebook</h2>
+            </a>
+            <br />
+            <a class="inline-block" href="https://instagram.com/vast.sg" target="_blank"
+              aria-label="Connect with us on Instagram" tabindex="0" tabposition="bottom" rel="noopener">
+              <h2>Instagram</h2>
+            </a>
+            <br />
+            <a class="inline-block" href="https://www.linkedin.com/company/vast-sg/" target="_blank"
+              aria-label="Connect with us on LinkedIn" tabindex="0" tabposition="bottom" rel="noopener">
+              <h2>LinkedIn</h2>
+            </a>
+            <br />
+            <a class="inline-block" href="https://dribbble.com/vastdesign" target="_blank"
+              aria-label="Connect with us on Dribbble" tabindex="0" tabposition="bottom" rel="noopener">
+              <h2>Dribbble</h2>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="m-4 grid gap-8 md:grid-cols-2 text-3xl lg:text-4xl font-light uppercase">
+        <nuxt-img v-if="image.url" format="webp" :src="image.url" sizes="sm:100vw md:100vw lg:100vw xl:100vw 2xl:100vw"
+          :width="image.dimensions.width" :height="image.dimensions.height" class="w-full rounded-lg object-cover"
+          loading="lazy" />
       </div>
     </section>
   </main>
