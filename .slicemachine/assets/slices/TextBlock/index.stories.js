3e0c99ca5f46f6bffaa453bda8fa9c0a9cbc3f2c
Aaron Choo <aaronchoo@gmail.com>
2022-05-03 11:02:06 +0800

    chore: updated slices

--- .slicemachine/assets/slices/TextBlock/index.stories.js
+++ .slicemachine/assets/slices/TextBlock/index.stories.js
@@ -18,7 +18,7 @@ export const _DefaultSlice = () => ({
   },
   data() {
     return {
-      mock: {"variation":"default-slice","name":"Default slice","slice_type":"text_block","items":[],"primary":{"title":[{"type":"heading1","text":"Orchestrate front-end convergence","spans":[]}],"description":[{"type":"paragraph","text":"Reprehenderit ex ea eu mollit eu sit. Cillum eu occaecat duis minim velit eiusmod nostrud ex culpa sint ad Lorem cillum amet velit. Irure exercitation proident eu officia amet in cillum sit.","spans":[]}]},"id":"_DefaultSlice"}
+      mock: {"variation":"default-slice","name":"Default slice","slice_type":"text_block","items":[],"primary":{"title":[{"type":"heading1","text":"Iterate end-to-end markets","spans":[]}],"description":[{"type":"paragraph","text":"Velit reprehenderit Lorem voluptate consectetur incididunt.","spans":[]}]},"id":"_DefaultSlice"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
