59843e1d253ff1172550d322a697176b20ef641d
Aaron Choo <aaronchoo@gmail.com>
2022-05-03 11:01:44 +0800

    feat: updated textblock

--- slices/TextBlock/index.vue
+++ slices/TextBlock/index.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="text-block section my-24 mx-4 lg:mx-40 grid grid-cols-12 gap-4">
     <h4
-      class="title tracking-tight leading-none uppercase text-2xl lg:text-3xl lg:leading-none col-span-10 col-start-3 lg:col-start-1 lg:col-span-4"
-    >
+      class="title tracking-tight leading-none uppercase text-2xl lg:text-3xl lg:leading-none col-span-10 col-start-3 lg:col-start-1 lg:col-span-4">
       {{ $prismic.asText(slice.primary.title) }}
     </h4>
     <!-- <prismic-rich-text :field="slice.primary.title" class="title uppercase" /> -->
-    <prismic-rich-text
-      :field="slice.primary.description"
-      class="leading-tight text-base lg:text-lg lg:leading-tight col-span-10 col-start-3 lg:col-start-5 lg:col-span-8"
-    />
+    <prismic-rich-text :field="slice.primary.description"
+      class="leading-tight text-base lg:text-lg lg:leading-tight col-span-10 col-start-3 lg:col-start-5 lg:col-span-8" />
   </div>
 </template>
 
@@ -28,7 +25,7 @@ export default {
 }
 </script>
 <style>
-.text-block p:nth-child(2) {
+.text-block p:nth-child(n+2) {
   text-indent: 4em;
 }
 </style>
