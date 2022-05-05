fda54140f67725ddd9de0bd76abf230c4eb8b304
Aaron Choo <aaronchoo@gmail.com>
2022-05-05 10:41:36 +0800

    feat: updated logo component

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="logo-block relative">
-    <div class="logo-block-v absolute left-0 top-0 w-1/4 h-full"></div>
-    <div class="logo-block-a absolute left-1/4 top-0 w-1/4 h-full"></div>
-    <div class="logo-block-s logo-block-s-bottom absolute left-1/2 top-1/2 w-1/4 h-1/2"></div>
-    <div class="logo-block-s logo-block-s-top absolute left-1/2 top-0 w-1/4 h-1/2"></div>
-    <div class="logo-block-t logo-block-t-top absolute left-3/4 top-0 w-1/4 h-1/2"></div>
-    <div class="logo-block-t logo-block-t-bottom absolute top-1/2 h-1/2"></div>
+    <div class="logo-blockform logo-blockform-v absolute left-0 top-0 w-1/4 h-full"></div>
+    <div class="logo-blockform logo-blockform-a absolute left-1/4 top-0 w-1/4 h-full"></div>
+    <div
+      class="logo-blockform logo-blockform-s logo-blockform logo-blockform-s-bottom absolute left-1/2 top-1/2 w-1/4 h-1/2">
+    </div>
+    <div class="logo-blockform logo-blockform-s logo-blockform-s-top absolute left-1/2 top-0 w-1/4 h-1/2"></div>
+    <div class="logo-blockform logo-blockform-t logo-blockform-t-top absolute left-3/4 top-0 w-1/4 h-1/2"></div>
+    <div class="logo-blockform logo-blockform-t logo-blockform-t-bottom absolute top-1/2 h-1/2"></div>
   </div>
 </template>
 
@@ -21,31 +23,31 @@ export default {
   height: auto;
 }
 
-.logo-block>div {
+.logo-blockform {
   background: var(--color);
   transition: transform 0.3s ease;
 }
 
-.logo-block-v {
+.logo-blockform-v {
   border-radius: 0 0 50% 50%;
   transform-origin: top right;
 }
 
-.logo-block-a {
+.logo-blockform-a {
   border-radius: 50% 50% 0 0;
   transform-origin: bottom right;
 }
 
-.logo-block-s-bottom {
+.logo-blockform-s-bottom {
   border-radius: 100ch;
   transform-origin: bottom left;
 }
 
-.logo-block-s-top {
+.logo-blockform-s-top {
   border-radius: 100ch 0 0 100ch;
 }
 
-.logo-block-t-bottom {
+.logo-blockform-t-bottom {
   left: 81.25%;
   width: 12.5%;
   transform-origin: bottom left;
