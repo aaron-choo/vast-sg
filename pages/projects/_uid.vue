0695e3b2e625179545f51e7ca4b8cb774d1b79f8
Aaron Choo <aaronchoo@gmail.com>
2022-05-12 02:43:11 +0800

    feat: updated project uid metatitle

--- pages/projects/_uid.vue
+++ pages/projects/_uid.vue
@@ -2,49 +2,146 @@
   <main>
     <section class="project-header">
       <div class="w-full overflow-hidden">
-        <div id="header-text" class="w-full flex flex-col justify-center p-4 py-52 pt-60 relative">
-          <h1 id="header-title" class="tracking-tight leading-none text-5xl lg:text-8xl uppercase">
-            <span v-for="(word, index) in titleWords" :key="index"
-              class="title-words inline-block overflow-hidden align-top"><span
-                v-for="(letter, index2) in Array.from(word)" :key="index2" class="inline-block">{{ letter
-                }}</span>&nbsp;</span>
+        <div
+          id="header-text"
+          class="w-full flex flex-col justify-center p-4 py-52 pt-60 relative"
+        >
+          <h1
+            id="header-title"
+            class="tracking-tight leading-none text-5xl lg:text-8xl uppercase"
+          >
+            <span
+              v-for="(word, index) in titleWords"
+              :key="index"
+              class="title-words inline-block overflow-hidden align-top"
+              ><span
+                v-for="(letter, index2) in Array.from(word)"
+                :key="index2"
+                class="inline-block"
+                >{{ letter }}</span
+              >&nbsp;</span
+            >
           </h1>
-          <p id="header-scope"
-            class="inline-block tag capitalize text-2xl lg:text-3xl transition duration-300 transform leading-6"
-            :class="{ 'has-scroll-over': scrollOver }">
-            <span class="summary text-sm lg:text-base uppercase inline-block mr-16 -mb-4">(Scope)</span><span
-              v-for="(tag, index) in tags" :key="index" class="inline-block serif font-light">
+          <p
+            id="header-scope"
+            class="
+              inline-block
+              tag
+              capitalize
+              text-2xl
+              lg:text-3xl
+              transition
+              duration-300
+              transform
+              leading-6
+            "
+            :class="{ 'has-scroll-over': scrollOver }"
+          >
+            <span
+              class="
+                summary
+                text-sm
+                lg:text-base
+                uppercase
+                inline-block
+                mr-16
+                -mb-4
+              "
+              >(Scope)</span
+            ><span
+              v-for="(tag, index) in tags"
+              :key="index"
+              class="inline-block serif font-light"
+            >
               {{ tag
               }}<template v-if="Object.keys(tags).length > 1">
-                <span v-if="index != Object.keys(tags).length - 1" class="sep">+</span>
+                <span v-if="index != Object.keys(tags).length - 1" class="sep"
+                  >+</span
+                >
               </template>
             </span>
           </p>
         </div>
-        <div v-if="image.url !== undefined" id="header-image-wrapper"
-          class="z-0 relative mx-4 overflow-hidden rounded-lg">
-          <nuxt-img v-if="image.url" id="header-image" format="webp" :src="image.url"
-            sizes="sm:100vw md:100vw lg:100vw xl:100vw 2xl:100vw" :width="image.dimensions.width"
-            :height="image.dimensions.height" class="w-full" loading="lazy" />
+        <div
+          v-if="image.url !== undefined"
+          id="header-image-wrapper"
+          class="z-0 relative mx-4 overflow-hidden rounded-lg"
+        >
+          <nuxt-img
+            v-if="image.url"
+            id="header-image"
+            format="webp"
+            :src="image.url"
+            sizes="sm:100vw md:100vw lg:100vw xl:100vw 2xl:100vw"
+            :width="image.dimensions.width"
+            :height="image.dimensions.height"
+            class="w-full"
+            loading="lazy"
+          />
         </div>
         <div class="description-section section my-24 px-4 lg:px-40">
           <p class="description-wrapper">
-            <span class="summary text-sm lg:text-base uppercase inline-block mr-16 mb-2 align-top">({{
-                $prismic.asText(summary)
-            }})</span><span v-for="(paragraph, index) in description" :key="index"
-              class="description font-light serif text-xl lg:text-3xl">{{ paragraph.text }}<br /></span>
+            <span
+              class="
+                summary
+                text-sm
+                lg:text-base
+                uppercase
+                inline-block
+                mr-16
+                mb-2
+                align-top
+              "
+              >({{ $prismic.asText(summary) }})</span
+            ><span
+              v-for="(paragraph, index) in description"
+              :key="index"
+              class="description font-light serif text-xl lg:text-3xl"
+              >{{ paragraph.text }}<br
+            /></span>
           </p>
         </div>
-        <div v-if="projectLink" class="project-link section my-24 px-4 lg:px-40">
+        <div
+          v-if="projectLink"
+          class="project-link section my-24 px-4 lg:px-40"
+        >
           <p class="description-wrapper mt-6">
-            <span class="summary text-sm lg:text-base uppercase inline-block mr-16 mb-2 align-top">(Live)</span><a
-              :href="projectLink" target="_blank" class="live-link uppercase font-light text-2xl lg:text-3xl">Visit
-              Website
-              <span class="live-link-arrow inline-block">↗</span></a>
+            <span
+              class="
+                summary
+                text-sm
+                lg:text-base
+                uppercase
+                inline-block
+                mr-16
+                mb-2
+                align-top
+              "
+              >(Live)</span
+            ><a
+              :href="projectLink"
+              target="_blank"
+              class="live-link uppercase font-light text-2xl lg:text-3xl"
+              >Visit Website
+              <span class="live-link-arrow inline-block">↗</span></a
+            >
           </p>
         </div>
         <hr class="h-px w-full opacity-20" />
-        <div id="header-separator" class="uppercase mt-4 mb-24 mx-4 pb-4 relative grid-cols-6 gap-x-4 grid">
+        <div
+          id="header-separator"
+          class="
+            uppercase
+            mt-4
+            mb-24
+            mx-4
+            pb-4
+            relative
+            grid-cols-6
+            gap-x-4
+            grid
+          "
+        >
           <div class="col-span-4 lg:col-span-2">
             <span>{{ $prismic.asText(client) }}</span>
           </div>
@@ -52,9 +149,14 @@
             <span>{{ date.substring(0, 4) }}</span>
           </div>
           <div class="col-span-6 lg:col-span-3 text-left lg:text-right">
-            <span v-for="(tag, index) in tags" :key="index">{{ tag
-            }}<template v-if="Object.keys(tags).length > 1"><span v-if="index != Object.keys(tags).length - 1"
-                  class="sep">+</span></template></span>
+            <span v-for="(tag, index) in tags" :key="index"
+              >{{ tag
+              }}<template v-if="Object.keys(tags).length > 1"
+                ><span v-if="index != Object.keys(tags).length - 1" class="sep"
+                  >+</span
+                ></template
+              ></span
+            >
           </div>
         </div>
       </div>
@@ -67,30 +169,105 @@
     <section id="more-projects" class="mt-40 overflow-hidden">
       <hr class="h-px w-full opacity-20 mb-10 lg:mb-20" />
       <div class="more-projects-container">
-        <p class="next-project-label text-sm lg:text-lg uppercase z-0 m-4 px-2 text-center">
+        <p
+          class="
+            next-project-label
+            text-sm
+            lg:text-lg
+            uppercase
+            z-0
+            m-4
+            px-2
+            text-center
+          "
+        >
           Next Project
         </p>
-        <div id="next-header-text"
-          class="w-full flex justify-center px-4 lg:px-40 py-4 lg:py-12 text-center z-10 relative">
-          <h3 id="next-header-title" class="tracking-tight leading-none text-5xl lg:text-8xl uppercase z-0">
-            <span v-for="(word, index) in nextProjectTitleWords" :key="index"
-              class="next-title-words inline-block overflow-hidden align-top"><span
-                v-for="(letter, index2) in Array.from(word)" :key="index2" class="inline-block">{{ letter
-                }}</span></span>
+        <div
+          id="next-header-text"
+          class="
+            w-full
+            flex
+            justify-center
+            px-4
+            lg:px-40
+            py-4
+            lg:py-12
+            text-center
+            z-10
+            relative
+          "
+        >
+          <h3
+            id="next-header-title"
+            class="
+              tracking-tight
+              leading-none
+              text-5xl
+              lg:text-8xl
+              uppercase
+              z-0
+            "
+          >
+            <span
+              v-for="(word, index) in nextProjectTitleWords"
+              :key="index"
+              class="next-title-words inline-block overflow-hidden align-top"
+              ><span
+                v-for="(letter, index2) in Array.from(word)"
+                :key="index2"
+                class="inline-block"
+                >{{ letter }}</span
+              ></span
+            >
           </h3>
         </div>
 
-        <nuxt-link v-if="nextProject !== undefined" id="next-project-section" :to="$prismic.linkResolver(nextProject)"
-          class="next-project-section project-panel pb-4 mx-4 lg:mx-40 block transition duration-700">
+        <nuxt-link
+          v-if="nextProject !== undefined"
+          id="next-project-section"
+          :to="$prismic.linkResolver(nextProject)"
+          class="
+            next-project-section
+            project-panel
+            pb-4
+            mx-4
+            lg:mx-40
+            block
+            transition
+            duration-700
+          "
+        >
           <div class="w-auto relative">
-            <div v-if="nextProjectImage.url !== undefined" id="next-header-image-wrapper"
-              class="z-0 overflow-hidden w-full rounded-lg h-full">
-              <div id="next-project-bg" class="h-full w-full pointer-events-none opacity-30 z-10"></div>
-              <nuxt-img v-if="nextProjectImage.url" id="next-header-image" format="webp" :src="nextProjectImage.url"
-                sizes="sm:100vw md:100vw lg:100vw xl:100vw 2xl:100vw" :width="nextProjectImage.dimensions.width"
+            <div
+              v-if="nextProjectImage.url !== undefined"
+              id="next-header-image-wrapper"
+              class="z-0 overflow-hidden w-full rounded-lg h-full"
+            >
+              <div
+                id="next-project-bg"
+                class="h-full w-full pointer-events-none opacity-30 z-10"
+              ></div>
+              <nuxt-img
+                v-if="nextProjectImage.url"
+                id="next-header-image"
+                format="webp"
+                :src="nextProjectImage.url"
+                sizes="sm:100vw md:100vw lg:100vw xl:100vw 2xl:100vw"
+                :width="nextProjectImage.dimensions.width"
                 :height="nextProjectImage.dimensions.height"
-                class="w-full h-full object-cover z-0 pointer-events-none transform transition duration-700"
-                loading="lazy" />
+                class="
+                  w-full
+                  h-full
+                  object-cover
+                  z-0
+                  pointer-events-none
+                  transform
+                  transition
+                  duration-700
+                "
+                loading="lazy"
+              />
             </div>
           </div>
         </nuxt-link>
@@ -225,7 +402,7 @@ export default {
   },
   head() {
     return {
-      title: this.$prismic.asText(this.page.data.metaTitle),
+      title: this.$prismic.asText(this.page.data.title) + ' — VAST',
       meta: [
         {
           hid: 'description',
